493c81c9588a1f4054e378a72beca81d494dd55f
youshengchang <changyousheng@yahoo.com>
2015-10-28 14:52:52 +0000

    Sign up new user

---
+++ spec/features/signing_users_up_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.feature "User Sign up" do
+    
+    scenario "with valid crendentials" do
+        
+        visit "/"
+        
+        click_link "Sign up"
+        fill_in "Email", with: "user@example.com"
+        fill_in "Password", with: "password"
+        fill_in "Password confirmation", with: "password"
+        click_button "Sign up"
+        
+        expect(page).to have_content("You have signed up successfully.")
+    end
+end
