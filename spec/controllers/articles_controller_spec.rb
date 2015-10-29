31b3303cccafdbf9ebdb8aca3abab5b444ef68b4
youshengchang <changyousheng@yahoo.com>
2015-10-29 20:05:36 +0000

    add article controller access control

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -1,12 +1,44 @@
 require 'rails_helper'
+require 'support/macros'
 
 RSpec.describe ArticlesController, type: :controller do
 
-  describe "GET #index" do
+  describe "GET #edit" do
+    before do
+      @john = User.create(email: "john@example.com", password: "password")
+    end
+    
+    context "owner is allowed to edit his/her articles" do
+      it "renders the edit template" do
+        
+        login_user @john
+        article = Article.create(title: "The first article", body: "The body of the first article", user: @john)
+        
+        get :edit, id: article
+        expect(response).to render_template :edit
+      end
+    end
+    
+    context "non-owner is not allowed to edit other's articles" do
+      
+      it "redirect to the root path" do
+        fred = User.create(email: "fred@example.com", password: "password")
+        
+        login_user fred
+        article = Article.create(title: "The First Article", body: "The body of the first article", user: @john)
+        
+        get :edit, id: article
+        expect(response).to redirect_to(root_path)
+        message = "You can only edit your own article."
+        expect(flash[:danger]).to eq message
+      end
+    end
+    
     it "returns http success" do
       get :index
       expect(response).to have_http_status(:success)
     end
+    
   end
 
 end
